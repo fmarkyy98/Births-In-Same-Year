01a6c01dc1a764296e9248d38a2f385abe2076cd
Fekete Márk <fmark98@gmail.com>
2019-10-31 14:24:44 +0100

    Ready to testing.

--- Births-In-Same-Year/Births-In-Same-Year/Births-In-Same-Year.cpp
+++ Births-In-Same-Year/Births-In-Same-Year/Births-In-Same-Year.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>    // std::sort
 
 using namespace std;
 
@@ -17,7 +18,7 @@ struct BirthsOnDate
 	int bithsCount;
 };
 
-bool within1Year(Date date1, Date date2)
+bool withinOneYear(Date date1, Date date2)
 {
 	bool result = false;
 	if (date1.year == date2.year)
@@ -50,6 +51,28 @@ bool within1Year(Date date1, Date date2)
 	}
 	return result;
 }
+/// <summary>
+/// Treu if date1 is earlyer in time than date2.
+/// </summary>
+/// <param name="date1">Represents the first date.</param>
+/// <param name="date2">Represents the second date.</param>
+bool compareTwoBirthsOnDate(BirthsOnDate date1, BirthsOnDate date2)
+{
+	bool result;
+	if (date1.date.year != date2.date.year)
+	{
+		result = date1.date.year < date2.date.year;
+	}
+	else if (date1.date.month != date2.date.month)
+	{
+		result = date1.date.month < date2.date.month;
+	}
+	else
+	{
+		result = date1.date.day < date2.date.day;
+	}
+	return result;
+}
 
 BirthsOnDate dates[MAX_N];
 
@@ -62,9 +85,26 @@ int main()
 		cin >> dates[i].date.year >> dates[i].date.month >> dates[i].date.day;
 	}
 
+	sort(dates, dates + n, compareTwoBirthsOnDate); // arrays inplicitly converts to the pointer, pointing the first element of the array, dates + n represents the pointer pointing after the last elements place in memory.
+	for (int i = 0; i < n; ++i)
+	{
+		int j = i;
+		while (j < n && withinOneYear(dates[i].date, dates[j].date))
+		{
+			++dates[i].bithsCount;
+			++j;
+		}
+	}
+	BirthsOnDate max = dates[0];
+	for (int i = 1; i < n; ++i)
+	{
+		if (dates[i].bithsCount > max.bithsCount)
+		{
+			max = dates[i];
+		}
+	}
 
-
-	std::cout << "Hello World!\n";
+	std::cout << max.date.year << " " << max.date.month << " " << max.date.day << endl;
 
 	return 0;
 }
